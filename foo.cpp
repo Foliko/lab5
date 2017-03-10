50be84d29e06ba93f2f44f4217ba13137b9f1d1a
Foliko <dz93@list.ru>
2017-03-10 11:40:38 +0300

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,5 +1,12 @@
 #include<catch.hpp>
 
-TEST_CASE("[foo]")
-  {
-  }
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[foo]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}

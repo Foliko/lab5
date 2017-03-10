ec1f15654fe2f07e81b3cb382a80ab9dd55f3a10
Foliko <dz93@list.ru>
2017-03-10 11:42:36 +0300

    Update bur.cpp

--- bur.cpp
+++ bur.cpp
@@ -1,5 +1,11 @@
 #include<catch.hpp>
 
-TEST_CASE("[bur]")
-{
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[bur]" ) {
+    REQUIRE( Factorial(4) == 24 );
+    REQUIRE( Factorial(5) == 120 );
+    REQUIRE( Factorial(6) == 720 );
 }

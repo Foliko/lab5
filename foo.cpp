ded6448cb57bf7a157626b6c00a5dd57c5483eb3
Foliko <dz93@list.ru>
2017-03-10 12:13:01 +0300

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,8 +1,5 @@
 #include"catch.hpp"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
 
 TEST_CASE( "Factorials are computed", "[foo]" ) {
     REQUIRE( Factorial(1) == 1 );
